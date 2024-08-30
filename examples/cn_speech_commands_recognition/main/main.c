768acc98276cbfd5f7f2ea0f0f3ae841faef4756
lvhaiyu <lvhaiyu@espressif.com>
2024-08-30 19:28:40 +0800

    feat(p4): add p4 bsp

--- examples/cn_speech_commands_recognition/main/main.c
+++ examples/cn_speech_commands_recognition/main/main.c
@@ -21,10 +21,8 @@
 #include "speech_commands_action.h"
 #include "model_path.h"
 
-#define LOG_MEMORY_SYSTEM_INFO         (1)
-#define LOG_TASK_SYSTEM_INFO           (1)
-#define LOG_TIME_INTERVAL_MS           (2000)
-#define SYS_TASKS_ELAPSED_TIME_MS      (2000)   // Period of stats measurement
+#include "bsp/esp-bsp.h"
+#include "ui.h"
 
 int detect_flag = 0;
 static esp_afe_sr_iface_t *afe_handle = NULL;
@@ -82,7 +80,7 @@ void feed_Task(void *arg)
 void detect_Task(void *arg)
 {
     esp_afe_sr_data_t *afe_data = arg;
-    int afe_chunksize = afe_handle->get_fetch_chunksize(afe_data);
+        int afe_chunksize = afe_handle->get_fetch_chunksize(afe_data);
     char *mn_name = esp_srmodel_filter(models, ESP_MN_PREFIX, ESP_MN_CHINESE);
     printf("multinet:%s\n", mn_name);
     esp_mn_iface_t *multinet = esp_mn_handle_from_name(mn_name);
@@ -113,6 +111,10 @@ void detect_Task(void *arg)
 #elif CONFIG_IDF_TARGET_ESP32S3 || CONFIG_IDF_TARGET_ESP32P4
         if (res->wakeup_state == WAKENET_DETECTED) {
             printf("WAKEWORD DETECTED\n");
+            bsp_display_lock(-1);
+            lv_label_set_text(ui_LabelSR, "请讲");
+            lv_obj_set_style_text_color(ui_LabelSR,  lv_color_hex(0xFEFEFE), LV_PART_MAIN | LV_STATE_DEFAULT);
+            bsp_display_unlock();
 	    multinet->clean(model_data);  // clean all status of multinet
         } else if (res->wakeup_state == WAKENET_CHANNEL_VERIFIED) {
             play_voice = -1;
@@ -120,7 +122,6 @@ void detect_Task(void *arg)
             printf("AFE_FETCH_CHANNEL_VERIFIED, channel index: %d\n", res->trigger_channel_id);
         }
 #endif
-
         if (detect_flag == 1) {
             esp_mn_state_t mn_state = multinet->detect(model_data, res->data);
 
@@ -133,6 +134,37 @@ void detect_Task(void *arg)
                 for (int i = 0; i < mn_result->num; i++) {
                     printf("TOP %d, command_id: %d, phrase_id: %d, string:%s prob: %f\n", 
                     i+1, mn_result->command_id[i], mn_result->phrase_id[i], mn_result->string, mn_result->prob[i]);
+                    bsp_display_lock(-1);
+                    lv_obj_set_style_text_color(ui_LabelSR,  lv_color_hex(0x4C986D), LV_PART_MAIN | LV_STATE_DEFAULT);
+                    if(strcmp(mn_result->string, " da kai dian deng") == 0) {
+                        lv_label_set_text(ui_LabelSR, "打开电灯"); 
+                    } else if(strcmp(mn_result->string, " guan bi dian deng") == 0) {
+                        lv_label_set_text(ui_LabelSR, "关闭电灯"); 
+                    } else if(strcmp(mn_result->string, " da kai kong tiao") == 0) {
+                        lv_label_set_text(ui_LabelSR, "打开空调"); 
+                    } else if(strcmp(mn_result->string, " guan bi kong tiao") == 0) {
+                        lv_label_set_text(ui_LabelSR, "关闭空调"); 
+                    } else if(strcmp(mn_result->string, " song feng mo shi") == 0) {
+                        lv_label_set_text(ui_LabelSR, "送风模式"); 
+                    } else if(strcmp(mn_result->string, " kai qi zhi re") == 0) {
+                        lv_label_set_text(ui_LabelSR, "开启制热"); 
+                    } else if(strcmp(mn_result->string, " kai qi tong feng") == 0) {
+                        lv_label_set_text(ui_LabelSR, "开启通风"); 
+                    } else if(strcmp(mn_result->string, " jun yun feng") == 0) {
+                        lv_label_set_text(ui_LabelSR, "均匀风"); 
+                    } else if(strcmp(mn_result->string, " you dian leng") == 0) {
+                        lv_label_set_text(ui_LabelSR, "有点冷"); 
+                    } else if(strcmp(mn_result->string, " you dian re") == 0) {
+                        lv_label_set_text(ui_LabelSR, "有点热"); 
+                    } else if(strcmp(mn_result->string, " dian fu re") == 0) {
+                        lv_label_set_text(ui_LabelSR, "电辅热"); 
+                    } else if(strcmp(mn_result->string, " zi dong") == 0) {
+                        lv_label_set_text(ui_LabelSR, "自动"); 
+                    } else {
+                        lv_label_set_text(ui_LabelSR, "未识别");
+                        lv_obj_set_style_text_color(ui_LabelSR,  lv_color_hex(0xB40000), LV_PART_MAIN | LV_STATE_DEFAULT);
+                    }
+                    bsp_display_unlock();
                 }
                 printf("\n-----------listening-----------\n");
             }
@@ -142,6 +174,10 @@ void detect_Task(void *arg)
                 printf("timeout, string:%s\n", mn_result->string);
                 afe_handle->enable_wakenet(afe_data);
                 detect_flag = 0;
+                bsp_display_lock(-1);
+                lv_label_set_text(ui_LabelSR, "开始测试");
+                lv_obj_set_style_text_color(ui_LabelSR,  lv_color_hex(0xFEFEFE), LV_PART_MAIN | LV_STATE_DEFAULT);
+                bsp_display_unlock();
                 printf("\n-----------awaits to be waken up-----------\n");
                 continue;
             }
@@ -155,12 +191,11 @@ void detect_Task(void *arg)
     vTaskDelete(NULL);
 }
 
-void app_main()
+static void sr_init_task(void *arg)
 {
     models = esp_srmodel_init("model");
     ESP_ERROR_CHECK(esp_board_init(16000, 2, 32));
     // ESP_ERROR_CHECK(esp_sdcard_init("/sdcard", 10));
-
 
     afe_handle = (esp_afe_sr_iface_t *)&ESP_AFE_SR_HANDLE;
     afe_config_t afe_config = AFE_CONFIG_DEFAULT();
@@ -178,171 +213,20 @@ void app_main()
     xTaskCreatePinnedToCore(&detect_Task, "detect", 8 * 1024, (void*)afe_data, 5, NULL, 1);
     xTaskCreatePinnedToCore(&feed_Task, "feed", 8 * 1024, (void*)afe_data, 5, NULL, 0);
 
-// #if defined  CONFIG_ESP32_S3_KORVO_1_V4_0_BOARD || defined CONFIG_ESP32_KORVO_V1_1_BOARD
-//     xTaskCreatePinnedToCore(&led_Task, "led", 3 * 1024, NULL, 5, NULL, 0);
-// #endif
-// #if defined  CONFIG_ESP32_S3_KORVO_1_V4_0_BOARD || CONFIG_ESP32_S3_KORVO_2_V3_0_BOARD || CONFIG_ESP32_KORVO_V1_1_BOARD
-//     xTaskCreatePinnedToCore(&play_music, "play", 2 * 1024, NULL, 5, NULL, 1);
-// #endif
+    bsp_display_start();
 
-    // You can call afe_handle->destroy to destroy AFE.
-//    vTaskDelay(2000 / portTICK_PERIOD_MS);
-//    task_flag = 0;
-//
-//    printf("destroy\n");
-//    afe_handle->destroy(afe_data);
-//    afe_data = NULL;
-//    printf("successful\n");
+    bsp_display_lock(-1);
+    ui_init();
+    bsp_display_unlock();
 
-#if LOG_MEMORY_SYSTEM_INFO
-    static char buffer[2048];
-    while (1) {
-        sprintf(buffer, "\t  Biggest /     Free /    Total\n"
-                " SRAM : [%8d / %8d / %8d]\n"
-                "PSRAM : [%8d / %8d / %8d]\n",
-                heap_caps_get_largest_free_block(MALLOC_CAP_INTERNAL),
-                heap_caps_get_free_size(MALLOC_CAP_INTERNAL),
-                heap_caps_get_total_size(MALLOC_CAP_INTERNAL),
-                heap_caps_get_largest_free_block(MALLOC_CAP_SPIRAM),
-                heap_caps_get_free_size(MALLOC_CAP_SPIRAM),
-                heap_caps_get_total_size(MALLOC_CAP_SPIRAM));
-        printf("------------ Memory ------------\n");
-        printf("%s\n", buffer);
+    bsp_display_backlight_on();
 
-        ESP_ERROR_CHECK(print_real_time_mem_stats());
-        printf("\n");
-
-        vTaskDelay(pdMS_TO_TICKS(LOG_TIME_INTERVAL_MS));
-    }
-#endif
-
+    vTaskDelete(NULL);
 }
 
-#if LOG_TASK_SYSTEM_INFO
-#define ARRAY_SIZE_OFFSET                   8   // Increase this if audio_sys_get_real_time_stats returns ESP_ERR_INVALID_SIZE
-
-#define audio_malloc    malloc
-#define audio_calloc    calloc
-#define audio_free      free
-#define AUDIO_MEM_CHECK(tag, x, action) if (x == NULL) { \
-        ESP_LOGE(tag, "Memory exhausted (%s:%d)", __FILE__, __LINE__); \
-        action; \
-    }
-
-const char *task_state[] = {
-    "Running",
-    "Ready",
-    "Blocked",
-    "Suspended",
-    "Deleted"
-};
-
-/** @brief
- * "Extr": Allocated task stack from psram, "Intr": Allocated task stack from internel
- */
-const char *task_stack[] = {"Extr", "Intr"};
-
-esp_err_t print_real_time_mem_stats(void)
+void app_main()
 {
-// #if (CONFIG_FREERTOS_VTASKLIST_INCLUDE_COREID && CONFIG_FREERTOS_GENERATE_RUN_TIME_STATS)
-//     TaskStatus_t *start_array = NULL, *end_array = NULL;
-//     UBaseType_t start_array_size, end_array_size;
-//     uint32_t start_run_time, end_run_time;
-//     uint32_t total_elapsed_time;
-//     uint32_t task_elapsed_time, percentage_time;
-//     esp_err_t ret;
-
-//     // Allocate array to store current task states
-//     start_array_size = uxTaskGetNumberOfTasks() + ARRAY_SIZE_OFFSET;
-//     start_array = (TaskStatus_t *)audio_malloc(sizeof(TaskStatus_t) * start_array_size);
-//     AUDIO_MEM_CHECK(TAG, start_array, {
-//         ret = ESP_FAIL;
-//         goto exit;
-//     });
-//     // Get current task states
-//     start_array_size = uxTaskGetSystemState(start_array, start_array_size, &start_run_time);
-//     if (start_array_size == 0) {
-//         ESP_LOGE(TAG, "Insufficient array size for uxTaskGetSystemState. Trying increasing ARRAY_SIZE_OFFSET");
-//         ret = ESP_FAIL;
-//         goto exit;
-//     }
-
-//     vTaskDelay(pdMS_TO_TICKS(SYS_TASKS_ELAPSED_TIME_MS));
-
-//     // Allocate array to store tasks states post delay
-//     end_array_size = uxTaskGetNumberOfTasks() + ARRAY_SIZE_OFFSET;
-//     end_array = (TaskStatus_t *)audio_malloc(sizeof(TaskStatus_t) * end_array_size);
-//     AUDIO_MEM_CHECK(TAG, start_array, {
-//         ret = ESP_FAIL;
-//         goto exit;
-//     });
-
-//     // Get post delay task states
-//     end_array_size = uxTaskGetSystemState(end_array, end_array_size, &end_run_time);
-//     if (end_array_size == 0) {
-//         ESP_LOGE(TAG, "Insufficient array size for uxTaskGetSystemState. Trying increasing ARRAY_SIZE_OFFSET");
-//         ret = ESP_FAIL;
-//         goto exit;
-//     }
-
-//     // Calculate total_elapsed_time in units of run time stats clock period.
-//     total_elapsed_time = (end_run_time - start_run_time);
-//     if (total_elapsed_time == 0) {
-//         ESP_LOGE(TAG, "Delay duration too short. Trying increasing SYS_TASKS_ELAPSED_TIME_MS");
-//         ret = ESP_FAIL;
-//         goto exit;
-//     }
-
-//     ESP_LOGI(TAG, "| Task              | Run Time    | Per | Prio | HWM       | State   | CoreId   | Stack ");
-
-//     // Match each task in start_array to those in the end_array
-//     for (int i = 0; i < start_array_size; i++) {
-//         for (int j = 0; j < end_array_size; j++) {
-//             if (start_array[i].xHandle == end_array[j].xHandle) {
-
-//                 task_elapsed_time = end_array[j].ulRunTimeCounter - start_array[i].ulRunTimeCounter;
-//                 percentage_time = (task_elapsed_time * 100UL) / (total_elapsed_time * portNUM_PROCESSORS);
-//                 ESP_LOGI(TAG, "| %-17s | %-11d |%2d%%  | %-4u | %-9u | %-7s | %-8x | %s",
-//                                 start_array[i].pcTaskName, (int)task_elapsed_time, (int)percentage_time, start_array[i].uxCurrentPriority,
-//                                 (int)start_array[i].usStackHighWaterMark, task_state[(start_array[i].eCurrentState)],
-//                                 start_array[i].xCoreID, 0);
-
-//                 // Mark that task have been matched by overwriting their handles
-//                 start_array[i].xHandle = NULL;
-//                 end_array[j].xHandle = NULL;
-//                 break;
-//             }
-//         }
-//     }
-
-//     // Print unmatched tasks
-//     for (int i = 0; i < start_array_size; i++) {
-//         if (start_array[i].xHandle != NULL) {
-//             ESP_LOGI(TAG, "| %s | Deleted", start_array[i].pcTaskName);
-//         }
-//     }
-//     for (int i = 0; i < end_array_size; i++) {
-//         if (end_array[i].xHandle != NULL) {
-//             ESP_LOGI(TAG, "| %s | Created", end_array[i].pcTaskName);
-//         }
-//     }
-//     printf("\n");
-//     ret = ESP_OK;
-
-// exit:    // Common return path
-//     if (start_array) {
-//         audio_free(start_array);
-//         start_array = NULL;
-//     }
-//     if (end_array) {
-//         audio_free(end_array);
-//         end_array = NULL;
-//     }
-//     return ret;
-// #else
-//     ESP_LOGW(TAG, "Please enbale `CONFIG_FREERTOS_VTASKLIST_INCLUDE_COREID` and `CONFIG_FREERTOS_GENERATE_RUN_TIME_STATS` in menuconfig");
-//     return ESP_FAIL;
-// #endif
-    return ESP_OK;
+    // bsp_extra_i2c_init();
+    /* Initialize display and LVGL */
+    xTaskCreate(sr_init_task, "sr_init_task", 4 * 1024, NULL, 5, NULL);
 }
-#endif
